44211650ddbd6e735b86bc8707ac22ed73e92fad
Kerm!t_ <151472054+kermitheman@users.noreply.github.com>
2025-12-03 18:32:44 +0000

    a

---
+++ build/CMakeFiles/SK_UI.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/dev/SK_UI/external/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/SK_UI.dir/external/imgui/backends/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/SK_UI.dir/external/imgui/backends/imgui_impl_glfw.cpp.obj.d"
+  "C:/dev/SK_UI/external/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/SK_UI.dir/external/imgui/backends/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/SK_UI.dir/external/imgui/backends/imgui_impl_opengl3.cpp.obj.d"
+  "C:/dev/SK_UI/external/imgui/imgui.cpp" "CMakeFiles/SK_UI.dir/external/imgui/imgui.cpp.obj" "gcc" "CMakeFiles/SK_UI.dir/external/imgui/imgui.cpp.obj.d"
+  "C:/dev/SK_UI/external/imgui/imgui_draw.cpp" "CMakeFiles/SK_UI.dir/external/imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/SK_UI.dir/external/imgui/imgui_draw.cpp.obj.d"
+  "C:/dev/SK_UI/external/imgui/imgui_tables.cpp" "CMakeFiles/SK_UI.dir/external/imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/SK_UI.dir/external/imgui/imgui_tables.cpp.obj.d"
+  "C:/dev/SK_UI/external/imgui/imgui_widgets.cpp" "CMakeFiles/SK_UI.dir/external/imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/SK_UI.dir/external/imgui/imgui_widgets.cpp.obj.d"
+  "C:/dev/SK_UI/src/main.cpp" "CMakeFiles/SK_UI.dir/src/main.cpp.obj" "gcc" "CMakeFiles/SK_UI.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ include/SK_UI.h
@@ -0,0 +1,13 @@
+//This is a Kerm!t made library
+//This library is for free use and should not under any circumstanses be sold for money to anyone
+//All of the downloads are available at https://github.com/kermitheman/SK-UI
+//Any Q? about the code ask on discord at @not_kermit.the.man
+
+//*************************************************** INCLUDING ********************************************************************//
+
+#include <glfw/glfw3.h>
+#include <iostream>
+
+//**************************************************** DEFINING ********************************************************************//
+
+void SayHello(){std::cout<<"hello";}
